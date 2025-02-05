7c30b1195d1cefd88a418a6c3285e9e2d345dc15
federico osandon <federicoaosandon@gmail.com>
2025-02-05 08:06:00 -0300

    update contact page function refresh and form create

--- src/app/page.js
+++ src/app/page.js
@@ -55,7 +55,10 @@ export default function Home() {
             width: "100%",
             borderRadius: "0",
             duration: 0.1
-        })        
+        })       
+        // para cambio de rutas evento de limpieza
+        // ScrollTrigger.getAll().forEach(trigger => trigger.kill())
+        ScrollTrigger.refresh() 
     })
 
     const handleShowForm = () => {
@@ -142,22 +145,172 @@ export default function Home() {
                 </footer>
             </div>   
             <section 
-                        ref={sectionFormRef}
-                        className="absolute top-0 left-0 flex flex-col items-center justify-center w-full min-h-screen bg-[#FFFFFF20]"
-                    >            
-                        <div 
-                            ref={formRef}
-                            className="flex items-center justify-center w-[95%] h-[200vh] bg-white text-black"
+                ref={sectionFormRef}
+                className="absolute top-0 left-0 flex flex-col items-center justify-center w-full min-h-screen bg-[#FFFFFF20]"
+            >            
+                <div 
+                    ref={formRef}
+                    className="flex items-center justify-center w-[95%] h-[200vh] bg-white text-black"
+                >
+                    <div 
+                        className="absolute top-4 right-4 font-bold text-3xl cursor-pointer"
+                        onClick={handleCloseForm} 
+                    > 
+                        X 
+                    </div>
+                    <div className="flex flex-col gap-16 w-[70%]">
+                        <h2
+                            size='80px'
+                            bold='font-semibold'
                         >
-                            <div 
-                                className="absolute top-4 right-4 font-bold text-5xl cursor-pointer"
-                                onClick={handleCloseForm} 
-                            > 
-                                X 
+                            <span>¡Cuéntanos</span> tus ideas!
+                        </h2>
+                        {/* <p>¿Tienes alguna pregunta? Estamos aquí para ayudarte. Completa el formulario y nos pondremos<br/> en contacto contigo lo antes posible. ¡Nos encantaría saber de ti!</p> */}
+                        <form className="flex flex-col gap-12 w-full">
+                            <input 
+                                type="text" 
+                                className="bg-transparent border-b-2 border-[#00000060] focus:outline-none focus:border-blue-500 w-full py-2 placeholder:text-[#000]"
+                                placeholder="Nombre"
+                            />
+
+                            <input 
+                                type="email" 
+                                className="bg-transparent border-b-2 border-[#00000060] focus:outline-none focus:border-blue-500 w-full py-2 placeholder:text-[#000] mt-4"
+                                placeholder="E-mail"
+                            />
+
+                            <div className="flex flex-col gap-4">
+                                <input  
+                                    type="text"                 
+                                    className="bg-transparent border-b-2 border-[#00000060] focus:outline-none focus:border-blue-500 w-full py-2 placeholder:text-[#000] mt-4"
+                                    placeholder="Teléfono"
+                                />
+                                <div className="flex">
+                                    <label className="text-[#00000080]">Contactar por: </label>
+                                    <input type="checkbox" />
+                                </div>
                             </div>
-                            Fromulario
-                        </div>
-                    </section>
+                
+
+                            <select 
+                                className="bg-transparent border-b-2 border-[#00000060] focus:outline-none focus:border-blue-500 w-full py-2 placeholder:text-[#000] mt-4"
+                            >
+                                {/* elija una opción  */}
+                                <option value="0">Asunto</option>
+                                <option value="1">Opción 1</option>
+                                <option value="2">Opción 2</option>
+                                <option value="3">Opción 3</option>
+                                <option value="4">Opción 4</option>
+                            </select>
+                            
+                            <textarea 
+                                type="text" 
+                                className="bg-transparent border-b-2 border-[#00000060] focus:outline-none focus:border-blue-500 w-full py-2 placeholder:text-[#000]"
+                                placeholder="Escribe tu mensaje"
+                            >
+
+                            </textarea>
+                            {/* input checkbox⁄ */}
+                            <label className="flex items-center space-x-2">
+                            
+                                <input 
+                                    type="checkbox" 
+                                    className="w-5 h-5 border-2"
+                                />
+                            <span className="">He leído y aceptro el <a href="#">Aviso legal</a> y <a href="#">Política de privacidad</a></span>
+                            </label>
+                            <div className=" w-full mt-12">
+                                <button
+                                    className="border border-black px-4 py-2 rounded-xl hover:bg-black hover:text-white"
+                                    onClick={handleCloseForm}
+                                >
+                                    Enviar
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </section>
+
+            {/* <section
+                ref={sectionFormRef}
+            >
+                <div 
+                     ref={formRef}
+                    className="flex flex-col items-center justify-start w-full bg-white text-black py-[10%] px-[5%]"
+                >
+                    <div className="flex flex-col gap-16 w-[70%]">
+                        <h2
+                            size='80px'
+                            bold='font-semibold'
+                        >
+                            <span>¡Cuéntanos</span> tus ideas!
+                        </h2>
+                        
+                        <form className="flex flex-col gap-12 w-full">
+                            <input 
+                                type="text" 
+                                className="bg-transparent border-b-2 border-[#00000060] focus:outline-none focus:border-blue-500 w-full py-2 placeholder:text-[#000]"
+                                placeholder="Nombre"
+                            />
+
+                            <input 
+                                type="email" 
+                                className="bg-transparent border-b-2 border-[#00000060] focus:outline-none focus:border-blue-500 w-full py-2 placeholder:text-[#000] mt-4"
+                                placeholder="E-mail"
+                            />
+
+                            <div className="flex flex-col gap-4">
+                                <input  
+                                    type="text"                 
+                                    className="bg-transparent border-b-2 border-[#00000060] focus:outline-none focus:border-blue-500 w-full py-2 placeholder:text-[#000] mt-4"
+                                    placeholder="Teléfono"
+                                />
+                                <div className="flex">
+                                    <label className="text-[#00000080]">Contactar por: </label>
+                                    <input type="checkbox" />
+                                </div>
+                            </div>
+                
+
+                            <select 
+                                className="bg-transparent border-b-2 border-[#00000060] focus:outline-none focus:border-blue-500 w-full py-2 placeholder:text-[#000] mt-4"
+                            >
+                              
+                                <option value="0">Asunto</option>
+                                <option value="1">Opción 1</option>
+                                <option value="2">Opción 2</option>
+                                <option value="3">Opción 3</option>
+                                <option value="4">Opción 4</option>
+                            </select>
+                            
+                            <textarea 
+                                type="text" 
+                                className="bg-transparent border-b-2 border-[#00000060] focus:outline-none focus:border-blue-500 w-full py-2 placeholder:text-[#000]"
+                                placeholder="Escribe tu mensaje"
+                            >
+
+                            </textarea>
+                           
+                            <label className="flex items-center space-x-2">
+                            
+                                <input 
+                                    type="checkbox" 
+                                    className="w-5 h-5 border-2"
+                                />
+                            <span className="">He leído y aceptro el <a href="#">Aviso legal</a> y <a href="#">Política de privacidad</a></span>
+                            </label>
+                            <div className=" w-full mt-12">
+                                <buttom
+                                    onClick={handleCloseForm}
+                                >
+                                    Enviar
+                                </buttom>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </section>   */}
         </>
     )
 }
